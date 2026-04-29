7f95adbc4d17990c3d2cdee55f651b84d2b3f9dc
Afanasiev Mihail <afanasev.ms@phystech.edu>
2026-04-29 12:01:31 +0300

    add header

---
+++ include/gitcringe.hpp
@@ -0,0 +1,57 @@
+#ifndef GITCRINGE_H
+#define GITCRINGE_H
+
+
+#include <iostream>
+#include <span>
+#include <string_view>
+#include <vector>
+
+
+
+namespace gcringe
+{
+    class Repo;
+
+
+    using commit_id_t = int64_t;
+
+
+    class Commit
+    {
+    public:
+        Commit(Repo repo, commit_id_t id);
+        ~Commit();
+
+
+        // restores File sytem to state of this commit
+        void RestoreFS();
+    };
+
+    class Transaction
+    {
+    public:
+        Transaction(Repo repo);
+        ~Transaction();
+
+        // Applyes changes and returnining new commit
+        Commit Apply();
+        // Add changes
+        void LoadFile(std::string path);
+    };
+
+    
+    class Repo
+    {
+    public:
+        
+        Repo(std::string path);
+        ~Repo();
+
+        // Start new commit
+        Transaction StartCommit(std::string path);
+    };
+}
+
+
+#endif
